02b6c3200650438767167a76d01efb3285467664
eboekenh <e.boekenheide@gmail.com>
2021-03-27 15:04:46 +0100

    Add files via upload

---
+++ eda.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+#############################################
+# GENERAL
+#############################################
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    # cat_cols + num_cols + cat_but_car = değişken sayısı.
+    # num_but_cat cat_cols'un içerisinde zaten.
+    # dolayısıyla tüm şu 3 liste ile tüm değişkenler seçilmiş olacaktır: cat_cols + num_cols + cat_but_car
+    # num_but_cat sadece raporlama için verilmiştir.
+
+    return cat_cols, cat_but_car, num_cols, num_but_cat
+
+
+
+
+#############################################
+# CATEGORICAL
+#############################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+#############################################
+# NUMERICAL
+#############################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+
+
+#############################################
+# TARGET
+#############################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
---
+++ helpers.py
@@ -0,0 +1,132 @@
+from mlxtend.frequent_patterns import apriori, association_rules
+
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def crm_data_prep(dataframe):
+    dataframe.dropna(axis=0, inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    return dataframe
+
+
+def create_invoice_product_df(dataframe):
+    return dataframe.groupby(['Invoice', 'StockCode'])['Quantity'].sum().unstack().fillna(0). \
+        applymap(lambda x: 1 if x > 0 else 0)
+
+import datetime as dt
+def create_cltv_p(dataframe):
+    today_date = dt.datetime(2011, 12, 11)
+
+    ## recency kullanıcıya özel dinamik.
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': [lambda date: (date.max()-date.min()).days,
+                                                                lambda date: (today_date - date.min()).days],
+                                                'Invoice': lambda num: num.nunique(),
+                                                'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    rfm.columns = rfm.columns.droplevel(0)
+
+    ## recency_cltv_p
+    rfm.columns = ['recency_cltv_p', 'T', 'frequency', 'monetary']
+
+    ## basitleştirilmiş monetary_avg
+    rfm["monetary"] = rfm["monetary"] / rfm["frequency"]
+
+    rfm.rename(columns={"monetary": "monetary_avg"}, inplace=True)
+
+
+    # BGNBD için WEEKLY RECENCY VE WEEKLY T'nin HESAPLANMASI
+    ## recency_weekly_cltv_p
+    rfm["recency_weekly_cltv_p"] = rfm["recency_cltv_p"] / 7
+    rfm["T_weekly"] = rfm["T"] / 7
+
+# KONTROL
+    rfm = rfm[rfm["monetary_avg"] > 0]
+
+    ## recency filtre (daha saglıklı cltvp hesabı için)
+    rfm = rfm[(rfm['frequency'] > 1)]
+
+    rfm["frequency"] = rfm["frequency"].astype(int)
+
+    # BGNBD
+    bgf = BetaGeoFitter(penalizer_coef=0.01)
+    bgf.fit(rfm['frequency'],
+            rfm['recency_weekly_cltv_p'],
+            rfm['T_weekly'])
+
+    # exp_sales_1_month
+    rfm["exp_sales_1_month"] = bgf.predict(4,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+    # exp_sales_3_month
+    rfm["exp_sales_3_month"] = bgf.predict(12,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+
+    # expected_average_profit
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(rfm['frequency'], rfm['monetary_avg'])
+    rfm["expected_average_profit"] = ggf.conditional_expected_average_profit(rfm['frequency'],
+                                                                             rfm['monetary_avg'])
+    # 6 aylık cltv_p
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       rfm['frequency'],
+                                       rfm['recency_weekly_cltv_p'],
+                                       rfm['T_weekly'],
+                                       rfm['monetary_avg'],
+                                       time=6,
+                                       freq="W",
+                                       discount_rate=0.01)
+
+    rfm["cltv_p"] = cltv
+
+    # minmaxscaler
+    scaler = MinMaxScaler(feature_range=(1, 100))
+    scaler.fit(rfm[["cltv_p"]])
+    rfm["cltv_p"] = scaler.transform(rfm[["cltv_p"]])
+
+    # rfm.fillna(0, inplace=True)
+
+    # cltv_p_segment
+    rfm["cltv_p_segment"] = pd.qcut(rfm["cltv_p"], 3, labels=["C", "B", "A"])
+
+    ## recency_cltv_p, recency_weekly_cltv_p
+    rfm = rfm[["recency_cltv_p", "T", "monetary_avg", "recency_weekly_cltv_p", "T_weekly",
+               "exp_sales_1_month", "exp_sales_3_month", "expected_average_profit",
+               "cltv_p", "cltv_p_segment"]]
+
+
+    return rfm
